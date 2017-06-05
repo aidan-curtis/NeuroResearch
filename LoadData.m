dd1ffb15f95f0498711d558082ca1de5eaec87ad
Aidan Curtis <aidancurtis@MacBook-Pro-8.local>
2017-06-05 10:47:47 -0500

    updated path

--- LoadData.m
+++ LoadData.m
@@ -1,8 +1,8 @@
 %% load data
-data = load('ta505_auditory.mat');
+data = load('../NeuroData/ta505_datasets/ta505_auditory.mat');
 ecog = data.nkdata.eeg;
 clear data;
 
 %% clean data
-
+plot(ecog(124, :))
 %remove bad channels
