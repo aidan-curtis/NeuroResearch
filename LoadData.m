e42b805ea560b691b3b8a787c0f8fd157c1af6b2
Aidan Curtis <aidancurtis@MacBook-Pro-8.local>
2017-06-05 18:08:23 -0500

    created filtering and tested different filters to find the best one

--- LoadData.m
+++ LoadData.m
@@ -7,17 +7,29 @@ ecog = double(data.nkdata.eeg);
 %select bad channels
 bad_channels = [125, 103, 97, 51];
 
+
+spectrogram(ecog(75, :))
+
+
 %% filter high gamma data
 
 start_amt = 1
 end_amt = 63800
-for channel = [1: 125]
+for channel = [1: 2]
+    
+%filter it
 b = fir1(480,[0.175 0.425]);
 H = freqz(b,1,400);
 max_ecog = max(abs(fft(ecog(channel, start_amt:end_amt))));
 filtered = filter(b, 1, ecog(channel, start_amt:end_amt));
+size(filtered)
+filtered = downsample(filtered, 2)
+spectrogram(filtered)
+% spectrogram(filtered)
+
+%making the power vector
 window_size = 50;
-[~, et] = size(filtered );
+[~, et] = size(filtered);
 
 for i = [1:(et/window_size)]
     %get power of window
@@ -32,7 +44,83 @@ disp(channel)
 end
 
 
-%% Plotting power vectors
+%% Get frequency vectors
+
+start_amt = 1
+end_amt = 638000
+for channel = [1: 2]
+    
+%filter it
+b = fir1(480,[0.175 0.425]);
+H = freqz(b,1,400);
+max_ecog = max(abs(fft(ecog(channel, start_amt:end_amt))));
+filtered = filter(b, 1, ecog(channel, start_amt:end_amt));
+size(filtered)
+filtered = downsample(filtered, 2)
+spectrogram(filtered)
+% spectrogram(filtered)
+
+%making the power vector
+window_size = 50;
+[~, et] = size(filtered);
+
+for i = [1:(et/window_size)]
+    %get power of window
+    log_power = 0;
+    for j = [1:window_size]
+       log_power = log_power+ (filtered((i-1)*window_size+j))^2;
+    end
+    E(i, channel) = log10(log_power);
+end
+disp('calculating vectors')
+disp(channel)
+end
+
+%% Get power of the signal at different frequencies
+
+centers = [70, 73.0, 79.5, 87.8, 96.9, 107.0, 118.1, 130.4, 144.0]
+sizes = []
+[~, sc] = size(centers);
+
+for a = [2: sc]
+    sizes(a-1) = centers(a)-centers(a-1)
+end
+
+%% Make filters for spectrums
 
 
-plot(E(: , 117))
+interval = ecog(1, 1100:2100);
+
+[~, N] = size(interval)
+
+
+%LENGTH MUST BE 100, OTHERWISE, YOU MUST CHANGE 70 and 170
+b = fir1(580,[70/N, 170/N]);
+H = freqz(b,1, N);
+
+
+filtered = filter(b, 1, interval)
+
+plot(filtered)
+
+
+
+
+% plot(interval)
+for channel = [1: 2]
+        
+        
+    
+%     for center = centers
+        center = 1;
+
+        bottom = centers(center)-sizes(center)/2;
+        top = centers(center)+size(center)/2;
+
+        [~, N] = size(interval);
+        
+
+%     end
+    
+end
+
