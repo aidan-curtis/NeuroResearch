8c53d3092fee1e56ec2e8fb7149d867f27233fe6
Dorian <dsk2@rice.edu>
2017-06-06 16:03:29 -0500

    %comenting

--- NotchFilter.m
+++ NotchFilter.m
@@ -21,17 +21,23 @@ plot (f, dbp);
 
 %% Notch filter attempt
 
+%sampling frequency
 fs = 1000;
+%desired harmonic to filter out
 fo = 60;
+%normalized frequency to filter out
 wo = fo/(fs/2);
+%"quality"
 q = 25;
+%bandwidth equasion based on quality
 bw = wo/q;
 
+%number of times the harmonic appears in the sampling range
 number_of_harmonics = floor(1/wo);
 
+%make the filters
 num = zeros(number_of_harmonics, 3);
 den = zeros(number_of_harmonics, 3);
-
 for i = 1:number_of_harmonics
     [num(i, :), den(i, :)] = iirnotch(i * wo, bw);
 end 
@@ -40,7 +46,8 @@ end
 %% filter each channel of deeg
 fdeeg = zeros (97, 638000);
 
-for i = 1:34
+%for every use_trial, throw it through every filter
+for i = 1:length(use_trials)
     fdeeg(use_trials(i), :) = filtfilt(num(1,:), den(1,:), deeg(use_trials(i), :));
     for j = 2:number_of_harmonics
         fdeeg(use_trials(i), :) = filtfilt(num(j,:), den(j,:), fdeeg(use_trials(i), :));
@@ -49,12 +56,12 @@ end
 
 %% Plot that shit
 [p, f] = periodogram (fdeeg(use_trials(1),:), [], [], 1000);
-for i = 2:34
+for i = 2:length(use_trials)
     [tempp, tempf] = periodogram (fdeeg(use_trials(i),:), [], [], 1000);
     p = p + tempp;
     f = tempf;
 end
-p = p ./ 34;
+p = p ./ length(use_trials);
 dbp = 10*log10(p);
 plot (f, dbp);
 
