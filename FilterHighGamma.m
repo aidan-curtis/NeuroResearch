c9ca9ccb9a1e4303a48d3130e11834e22d207f25
Aidan Curtis <aidancurtis@MacBook-Pro-8.local>
2017-06-06 14:23:14 -0500

    Finished high gamma filtering and building power vectors

--- FilterHighGamma.m
+++ FilterHighGamma.m
@@ -1,12 +1,15 @@
 %% filter high gamma data
 
 start_amt = 1;
-end_amt = 63800;
-for channel = [1: size(data.eeg,2)]
+end_amt = size(data.eeg,2);
+filtered = zeros(size(data.eeg,1), size(data.eeg,2));
+for channel = [1: size(data.eeg,1)]
     %filter it
-    b = fir1(480,[0.175 0.425]);
-    H = freqz(b,1,400);
-    max_ecog = max(abs(fft(data.eeg(channel, start_amt:end_amt))));
-    filtered(channel) = filter(b, 1, data.eeg(channel, start_amt:end_amt));
-    spectrogram(filtered);
+    b = fir1(4000,[2*70/1000, 2*170/1000]);
+    H = freqz(b,1,1000);
+    size(filter(b, 1, data.eeg(channel, start_amt:end_amt)));
+    filtered(channel, :) = filter(b, 1, data.eeg(channel, start_amt:end_amt));
 end
+%%
+
+periodogram(filtered(1,:))
