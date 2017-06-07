151e3a67a3eb41a1d336ad30cc00b8689847ee19
Dorian <dsk2@rice.edu>
2017-06-07 09:56:24 -0500

    Updated load data to make both a_data for aud and c_data for com

--- LoadData.m
+++ LoadData.m
@@ -1,20 +1,25 @@
 %% load data
 clear
-data = load('../NeuroData/ta505_datasets/ta505_auditory.mat');
+a_data = load('../NeuroData/ta505_datasets/ta505_auditory.mat');
+c_data = load('../NeuroData/ta505_datasets/ta505_common.mat');
 bad_channels = load('../NeuroData/ta505_datasets/ta505_bad_channels.mat');
 
-data = data.nkdata;
+a_data = a_data.nkdata;
+c_data = c_data.nkdata;
 bad_channels = bad_channels.bad_channels;
-data.fs = 1000;
-
+a_data.fs = 1000 / c_data.ms_per_sample;
+c_data.fs  = 1000 / c_data.ms_per_sample;
 %% Sudha's code
 % use trials <-- indices of trials not marked by experiemnters as
 % noisy/innacurate/technical failure
 % use_times <-- timestamps for use_trials articulations
 
+a_use_trials = find(a_data.accuracy(:).*a_data.tech(:).*a_data.noise(:));
+a_use_times = a_data.articulation(a_use_trials);
+a_data.eeg = double(a_data.eeg(bad_channels.auditory,:));
+a_data.ch_names = a_data.ch_names(bad_channels.auditory,:);
 
-use_trials = find(data.accuracy(:).*data.tech(:).*data.noise(:));
-use_times = data.articulation(use_trials);
-data.eeg = double(data.eeg(bad_channels.auditory,:));
-data.ch_names = data.ch_names(bad_channels.auditory,:);
-
+c_use_trials = find(c_data.accuracy(:).*c_data.tech(:).*c_data.noise(:));
+c_use_times = c_data.articulation(c_use_trials);
+c_data.eeg = double(c_data.eeg(bad_channels.common,:));
+c_data.ch_names = c_data.ch_names(bad_channels.common,:);
