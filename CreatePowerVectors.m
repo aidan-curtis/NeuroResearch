e7d70cfe0cc2c106b5e7c663936880138b858f40
Aidan Curtis <aidancurtis@MacBook-Pro-8.local>
2017-06-07 12:39:38 -0500

    power vectors

--- CreatePowerVectors.m
+++ CreatePowerVectors.m
@@ -1,17 +1,15 @@
   
 
 %create window for trial: ONLY USE_TRIALS
-window_start = 1
-window_end = 20
-for channel = [1: size(data.eeg,1)]
-% making the power vector
+window_start = data.pulse_on(1);
+window_end = data.pulse_on(2);
 
-    et = size(data.filtered, 2);
-   
-    %get power of window
+for channel = [1: size(data.filtered,1)]
     log_power = 0;
-    for j = [1:window_size]
+    total = 0;
+    for j = [window_start:window_end]
         log_power = log_power + (filtered(channel, (trial-1)*window_size+j))^2;
+        total = total + 1;
     end
-    E(trial, channel) = log10(log_power);
+    E(trial, channel) = log10(log_power/total);
 end
