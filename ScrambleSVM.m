ea375aa6392f63401f7e710a0c435f3c7ce7b079
Dorian <dsk2@rice.edu>
2017-06-08 12:46:41 -0500

    ugh

--- ScrambleSVM.m
+++ ScrambleSVM.m
@@ -1,13 +1,13 @@
 
-model_array = E(:, data.good_channels);
+model_array = E(:, data.good_channels,1);
 res = double(transpose(data.use_scramble));
 tmpl={'s','n'};
 model_logical=logical(res);
 model_response = transpose(tmpl(b+1));
 mdl = fitcsvm(model_array(1:180, :),model_response(1:180, :),'KernelFunction','RBF','Standardize',true,'ClassNames',{'s','n'});
 
-total = 180
-success = 0
+total = 180;
+success = 0;
 for response_el = cell2mat(predict(mdl, model_array(181:end, :)))'
 
     total = total+1;
