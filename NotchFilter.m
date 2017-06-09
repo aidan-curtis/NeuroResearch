fca28c6237e7bca6c4ba83d50d277b8080222e94
Dorian <dsk2@rice.edu>
2017-06-09 11:17:16 -0500

    windows.m

---
+++ NotchFilter.m
@@ -0,0 +1,55 @@
+%% Notch filter attempt
+disp('making notch filters...');
+%desired harmonic to filter out
+fo = 60;
+%normalized frequency to filter out
+wo = fo/(data.fs/2);
+%"quality"
+q = 25;
+%bandwidth equasion based on quality
+bw = wo/q;
+
+%number of times the harmonic appears in the sampling range
+number_of_harmonics = floor(1/wo);
+
+%make the filters
+num = zeros(number_of_harmonics, 3);
+den = zeros(number_of_harmonics, 3);
+for filter = 1:number_of_harmonics
+    [num(filter, :), den(filter, :)] = iirnotch(filter * wo, bw);
+end 
+
+disp('notch filters made');
+
+%% filter each channel of deeg
+disp('applying notch filters...');
+data.notch_filtered_eeg = double(data.eeg);
+
+%for every use_trial, throw it through every filter
+for channel = 1:size(data.notch_filtered_eeg, 1)
+    for filter = 1:number_of_harmonics
+        fprintf('.');
+        data.notch_filtered_eeg(channel, :) = filtfilt(num(filter,:), den(filter,:), data.notch_filtered_eeg(channel, :));
+    end
+    fprintf('\n');
+end
+
+
+data.eeg = data.notch_filtered_eeg;
+
+clearvars num den bw number_of_harmonics q wo fo;
+clear channel
+clear filter
+disp('notch filters applies');
+%% Plot that shit
+% disp('plotting...');
+% [p, f] = periodogram (data.notch_filtered_eeg(1,:), [], [], 1000);
+% for i = 2:size(data.notch_filtered_eeg, 1)
+%     [tempp, tempf] = periodogram (data.notch_filtered_eeg(i,:), [], [], 1000);
+%     p = p + tempp;
+%     f = tempf;
+% end
+% p = p ./ size(data.notch_filtered_eeg, 1);
+% dbp = 10*log10(p);
+% plot (f, dbp);
+% disp('plotted');
---
+++ Windows.m
@@ -0,0 +1,12 @@
+WINDOW_LENGTH = 2500;
+MS_BETWEEN_WINDOWS = 1000;
+
+number_of_windows = floor((size(data.filtered, 2) - WINDOW_LENGTH)/MS_BETWEEN_WINDOWS);
+
+
+for channel = 1:size(data.filtered, 1)
+    for window = 1:number_of_windows
+        window_start = ((window - 1) * MS_BETWEEN_WINDOWS)+1;
+        data.windows(channel, window, :) = data.filtered( channel, window_start: window_start + WINDOW_LENGTH); 
+    end
+end
